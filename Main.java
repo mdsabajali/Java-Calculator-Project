011bd0ac509cf48f66b6412507592bb83c4e3b63
Md Sabaj Ali <145532131+mdsabajali@users.noreply.github.com>
2023-09-20 19:37:23 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double a = sc.nextDouble();
+        System.out.println("Enter the second number");
+        double b = sc.nextDouble();
+        System.out.println("Enter the operator you want to calculate +,-,*,/,%");
+        char op = sc.next().charAt(0);
+
+
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+        }
+    }
+}
